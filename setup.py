87f48163c1e6204c0e4eede982ea8b019e0096b1
Oleg Broytman <phd@phdru.name>
2023-12-09 21:51:26 +0300

    Feat(setup.py): Install `rfr` entry point

--- setup.py
+++ setup.py
@@ -30,4 +30,9 @@ setup(
     license="GPLv3",
     classifiers=rfr_classifiers,
     python_requires=">=3.4.0",
+    entry_points={
+        'console_scripts': [
+            'rfr = rfr:main'
+        ]
+    },
 )
